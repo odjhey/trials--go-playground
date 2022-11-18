f32858276d3a5698a343b855e5daa6e35c0f8955
Odelon Jhey Pacalso <odjhey@gmail.com>
2022-11-18 08:33:59 +0800

    put back simple sample

---
+++ zeasyncawait/mainsimple.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"zeasyncawait/async"
+)
+
+func main() {
+	fmt.Println("main")
+	// call async
+	promiseLikeChannel := async.Call(func() interface{} {
+		return "Hello World!"
+	})
+
+	// simulate await
+	message := <-promiseLikeChannel
+
+	fmt.Println(message)
+}
