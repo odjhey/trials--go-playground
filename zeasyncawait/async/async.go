0abd98c338e3e13288833020b5ebfa1a5cbef4f0
Odee <odjhey@gmail.com>
2022-11-18 09:12:54 +0800

    Update async.go

--- zeasyncawait/async/async.go
+++ zeasyncawait/async/async.go
@@ -18,3 +18,6 @@ func Call(fn asyncFn) chan interface{} {
 // 1. Make func type aware with generics
 // 2. Do we have a limit on the number of channels we can create?
 // 3. Is creating channels cheap? can we do use it like a regular var
+
+// looks to me like this is a possible usable construct
+// now have to find a good library that achieves the same dev exp
