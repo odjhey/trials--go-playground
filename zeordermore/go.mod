be45ceeeef4941aa1d8451fe6b2819b77636981b
Odelon Jhey Pacalso <odjhey@gmail.com>
2022-11-23 05:43:31 +0800

    feat: init zeorder

---
+++ zeordermore/go.mod
@@ -0,0 +1,3 @@
+module zeordermore
+
+go 1.16
---
+++ zeordermore/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+)
+
+func main() {
+	fmt.Println("ei start!")
+
+	err := start()
+	if err != nil {
+		panic(err)
+	}
+
+}
+
+func start() error {
+
+	shutdown := make(chan os.Signal, 1)
+	signal.Notify(shutdown, syscall.SIGINT)
+
+	api := http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  8 * time.Second,
+		WriteTimeout: 8 * time.Second,
+	}
+
+	serverError := make(chan error, 1)
+	go func() {
+
+		serverError <- api.ListenAndServe()
+
+	}()
+
+	select {
+	case sig := <-shutdown:
+		fmt.Println("Shutting down with signal", sig)
+		// TODO: add a "drain" activity before killing
+		return nil
+	case err := <-serverError:
+		return err
+	}
+
+}
